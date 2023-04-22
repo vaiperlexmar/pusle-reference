7db76be1735eb3a9f1d1f1cc2d2c565b5eeeedc3
vaiperlexmar <vaiperlexmar@gmail.com>
2023-04-22 22:59:55 +0300

    add style for main block, few changes in js functions of animations

--- index.js
+++ index.js
@@ -4,29 +4,45 @@ const menu = document.getElementById("nav-links");
 const menuLinks = document.getElementsByClassName("nav-link");
 const mainBlock = document.getElementsByTagName("main")[0];
 const footerBlock = document.getElementsByTagName("footer")[0];
+const hamburgerButton = document.getElementsByClassName("hamburger")[0];
 $(menuLinks).css("opacity", 0);
 
 function openMenu() {
+  function fadeQueryMenuAnimation() {
+    for (var i = 0; i < menuLinks.length; i++) {
+      $(menuLinks[i])
+        .delay(i * 150)
+        .animate(
+          { opacity: 1 },
+          {
+            duration: 400,
+            complete: function () {
+              $(this).stop();
+            },
+          }
+        );
+    }
+  }
+
+  function fadeOutMenuAnimation() {
+    $(menu).fadeOut("slow");
+  }
+
   function contentRemoving() {
     mainBlock.style.display = "none";
     footerBlock.style.display = "none";
     mainBlock.replaceWith(menu);
-    menu.classList.remove("disappearance-animation");
+    fadeQueryMenuAnimation();
 
-    for (var i = 0; i < menuLinks.length; i++) {
-      $(menuLinks[i])
-        .delay(i * 150)
-        .animate({ opacity: 1 }, 400);
-    }
     menu.style.display = "block";
   }
 
   function contentAdding() {
+    fadeOutMenuAnimation();
     menu.replaceWith(mainBlock);
     mainBlock.style.display = "block";
     footerBlock.style.display = "block";
     menu.style.display = "none";
-    menu.classList.add("disappearance-animation");
   }
 
   if (menu.style.display === "block") {
